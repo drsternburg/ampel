50029ecc1b12db2823af8561da7cd355a058a25d
Matthias Schultze-Kraft <schultze-kraft@bccn-berlin.de>
2020-09-14 19:47:13 +0200

    updated and added functions

---
+++ acquisition/amp_findCoutThresh_v2.m
@@ -0,0 +1,55 @@
+
+function [thresh_move,thresh_idle] = amp_findCoutThresh_v2(C,target_ISI)
+
+%%
+Nt = length(C);
+X = [];
+for jj = 1:Nt
+    tind = C{jj}.t<=0;
+    x = C{jj}.x(tind);
+    X = cat(1,X,x');
+end
+
+Nth = 100;
+thresh = linspace(prctile(X,1),prctile(X,99),Nth);
+
+Ncr = zeros(Nth,2);
+minp = 1;
+for kk = 1:Nth
+    
+    ind1 = find(X<thresh(kk),1,'first');
+    ind2 = find(X<thresh(kk),1,'last');
+    x = X(ind1:ind2);
+    df = diff(sign(x-thresh(kk)));
+    t1 = find(df==2);
+    t2 = find(df==-2);
+    Ncr(kk,1) = sum((t2-t1)>minp);
+    
+    ind1 = find(X>thresh(kk),1,'first');
+    ind2 = find(X>thresh(kk),1,'last');
+    x = X(ind1:ind2);
+    df = diff(sign(x-thresh(kk)));
+    t1 = find(df==2);
+    t2 = find(df==-2);
+    Ncr(kk,2) = sum((t1-t2)>minp);
+    
+end
+
+Ttot = length(X)/100;
+R = Ttot./Ncr;
+
+%%
+figure
+plot(thresh,Ttot./Ncr)
+
+%%
+thresh_move = thresh(find(diff(sign(R(:,1) - target_ISI))==2));
+thresh_idle = thresh(find(diff(sign(R(:,2) - target_ISI))==-2));
+
+
+
+
+
+
+
+
--- acquisition/amp_runExperiment.m
+++ acquisition/amp_runExperiment.m
@@ -16,11 +16,11 @@ amp_startRecording('selfpaced')
 %% Block 2: traffic-light (random)
 opt.feedback_params.mode = int16(2);
 opt.feedback_params.listen_to_keyboard = int16(0);
-opt.feedback_params.end_pause_counter_type = int16(3); % idle lights
+opt.feedback_params.end_pause_counter_type = int16(6); % random lights
 opt.feedback_params.end_after_x_events = int16(50);
 opt.feedback_params.pause_every_x_events = int16(25);
 opt.feedback_params.cue_waittime = amp_drawCueTimes(100,[4000 10000]);
-opt.feedback_params.trial_assignment = int16(amp_drawTrialAssignments(100,[1/3 1/3 1/3 0 0 0]));
+opt.feedback_params.trial_assignment = int16(amp_drawTrialAssignments(100,[1/4 1/4 1/2]));
 amp_startRecording('random')
 
 %% Setup BCI
@@ -35,8 +35,8 @@ save([fullfile(BTB.Tp.Dir,opt.session_name) '_' BTB.Tp.Code '_opt'],'opt')
 %% Block 3: traffic-light (BCI)
 opt.feedback_params.mode = int16(3);
 opt.feedback_params.listen_to_keyboard = int16(0);
-opt.feedback_params.end_pause_counter_type = int16(2); % move lights
+opt.feedback_params.end_pause_counter_type = int16(7); % BCI lights
 opt.feedback_params.end_after_x_events = int16(40);
 opt.feedback_params.pause_every_x_events = int16(40);
-opt.feedback_params.trial_assignment = int16(amp_drawTrialAssignments(250,ones(1,6)/6));
+opt.feedback_params.trial_assignment = int16(amp_drawTrialAssignments(250,[1/8 1/8 1/4 1/8 1/8 1/4])+3);
 amp_startRecording('bci')
